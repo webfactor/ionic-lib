1d35465248650a942b1175ac50c7c0fdeda5b86f
Christian Kasperbauer <christian.kasperbauer@webfactor.de>
2018-03-26 14:40:39 +0200

    add exports

--- src/lib/src/index.ts
+++ src/lib/src/index.ts
@@ -1,1 +1,6 @@
 export { WfmIonicLibModule } from './lib.module';
+
+export * from './components/app-version/app-version';
+export * from './components/status-message/status-message';
+
+export * from './providers/api';
