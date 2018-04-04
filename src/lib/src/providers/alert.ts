38f6b567b003b16daf6bc4cb4927fbe5f6d9e54f
Christian Kasperbauer <christian.kasperbauer@webfactor.de>
2018-04-04 12:20:49 +0200

    [EH #8463] fix import

--- src/lib/src/providers/alert.ts
+++ src/lib/src/providers/alert.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class AlertService {
